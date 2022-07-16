b5c0ea688a567384abee9b389cad1cf70ffa9deb
lucasrpatten <68756367+lucasrpatten@users.noreply.github.com>
2022-07-15 21:12:20 -0600

    slide creation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import lucas0 from './lucas0.jpg';
+import lucas1 from './lucas1.jpg';
+import lucas2 from './lucas2.jpg';
 
 function App() {
+  const [slideImg, setSlideImg] = useState(0)
+
+  const slideSwitch = (direction="forward") => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="title-container">
+        <span id='title'>
+          Lucas Patten
+        </span>
+      </div>
+      <div id='imageslide'>
+        <span id="slide-back">&lt;</span>
+        <img className='backgroundImage' src={lucas0} alt="background" />
+        <img className="image" src={lucas0} alt='Lucas'/>
+        <span id="slide-forward">&gt;</span>
+      </div>
+    </>
   );
 }
 
